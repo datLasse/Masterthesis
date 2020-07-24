9997e5ff45f06839943996825dbd51bdfacf37c1
Lasse <bauer.finn@outlook.de>
2020-07-24 15:26:47 +0200

    realxation method

--- weaver/Coding Help/test.py
+++ weaver/Coding Help/test.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-a = np.array([1,2,3,4,5])
-print(a)
-print(a.shape)
-print(a[1:-1])
-print(a[0:-2])
-print(a[1:-1]-a[0:-2])
-print(a[0:2])
-print(a[2:])
+a = np.array([2*10**19,5*10**22,1*10**23,2*10**23,2*10**23])
+
+print(a[2:]-2.*a[1:-1]+a[0:-2])
